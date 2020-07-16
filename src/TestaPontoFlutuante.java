66439c9fd3b494aa05dd8b246f2020c8eb3431f5
Elton Standerski <eltonstand@gmail.com>
2020-07-16 16:58:17 -0300

    Aula 04: int e double

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,21 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("meu salario é " + salario);
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2.0;
+		System.out.println(novaTentativa);
+		
+	}
+
+}
